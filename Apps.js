1015ebd3117fbe1ce0b19921044747884782d033
unknown <praneeth.vadrevu@gmail.com>
2023-09-28 16:29:37 +0530

    Initiated the app to using react without using the create react app command

--- Apps.js
+++ Apps.js
@@ -1,30 +1,6 @@
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello World from React!"
-// );
-/* The react element 'Heading' above is nothing but a JS object. The createElement method of the 
-takes the inputs and creates a JS object using the data.
-
-*/
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-/*render method is responsible for converting the object created above into markup and render it
-on the browser. 
-    ReactElement(Object) => HTML(browser understands)
-*/
-
-// creating nested elements in react
-
-/* <div id="parent">
-    <div id="child"> 
-        <h1> I'm an H1 tag </h1> 
-    </div>
-     <div id="child"> 
-        <h1> I'm an H1 tag </h1> 
-    </div>
-</div>*/
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
@@ -38,4 +14,3 @@ const parent = React.createElement("div", { id: "parent" }, [
 ]);
 
 root.render(parent);
-console.log(parent);
